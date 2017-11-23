c93370d255125962769fef4668f70e561f64483e
root <root@ip-172-31-5-3.ap-northeast-2.compute.internal>
2017-11-23 05:53:40 +0000

    시뮬레이션 PHP 파일 업데이트ㅈㅂ

--- Simulator/simul.php
+++ Simulator/simul.php
@@ -29,24 +29,6 @@
    	<p>모두의 엘리베이터</p>
 </div>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script>
-            $(document).ready(function(){
-            $('#myDropDown').change(function(){
-                //Selected value
-                var inputValue = $(this).val();
-                //alert("value in js "+inputValue);
-
-                //Ajax for calling php function
-                $.post('submit.php', { dropdownValue: inputValue }, function(data){
-                    //alert('ajax completed. Response:  '+data);
-                    //do after submission operation in DOM
-                });
-            });
-        });
-        </script>
-
-
 해당 엘리베이터 선택
 <?php
     	$host = 'localhost';
@@ -54,21 +36,62 @@
     	$pw = 'root';
     	$dbName = 'capstone';
 	
-mysql_connect($host, $user, $pw);
-mysql_select_db($dbName);
+	mysql_connect($host, $user, $pw);
+	mysql_select_db($dbName);
 
-$sql = "SELECT Building_Id, Building_Name FROM building";
-$result = mysql_query($sql);
+	$sql = "SELECT Building_Id, Building_Name FROM building";
+	$result = mysql_query($sql);
 
-echo "<select id='myDropDown'>";
-while ($row = mysql_fetch_array($result)) {
-    echo "<option value='" . $row['Building_Id'] . "'>" . $row['Building_Name'] . "</option>";
-}
-echo "</select>";
-
+	echo "<select id='myDropDown' name = 'myDropDown'>";
+	while ($row = mysql_fetch_array($result)) {
+    		echo "<option value='" . $row['Building_Id'] . "'>" . $row['Building_Name'] . "</option>";
+	}
+	echo "</select>";
+	
+	//여기가 위에가 드롭다운 리스트 만드는 php
 
 ?>
 
+<form method ="post" action = "submit.php">
+<td> <input type="submit" value = "보기"/></td>
+엘리베이터 아이디<p id="ele_id"></p>
+
+</form>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script>//드롭다운 리스트가 변경되면 일어나는 스크립트
+            $(document).ready(function(){
+            $('#myDropDown').change(function(){
+                //Selected value
+                var inputValue = $(this).val();
+                //alert("value in js "+inputValue);
+		//document.write(inputValue);
+                document.getElementById('ele_id').innerHTML = inputValue;
+		//Ajax for calling php function
+		
+			
+		$.post('submit.php', { dropdownValue: inputValue }, function(data){
+                    //alert('ajax completed. Response:  '+data);
+                    //do after submission operation in DOM
+	//			var result = $('.result').html(data);
+		});
+            });
+        });
+</script>
+<?php
+	
+function processDrpdown($selectedVal) {
+    //echo "Selected value in php<BR> ".$selectedVal;
+	echo "1";
+
+}
+
+if ($_POST['dropdownValue']){
+    //call the function or execute the code
+    processDrpdown($_POST['dropdownValue']);
+	echo "@@@@@@@@@@@@";
+        //echo "Selected value in php<BR> ".$selectedVal;
+}	
+?>
 
 </body>
 </html>
