d94edf247e28bd22bb3a90b0b98eb27414ba5452
NamHwaSu <namhs93@ajou.ac.kr>
2017-12-11 18:32:29 +0900

    안드로이드 최종본

--- userRegister.php
+++ userRegister.php
@@ -1,38 +1,18 @@
 <?php
-
-        $db_host = "localhost";
-        $db_user = "root";
-        $db_password = "root";
-        $db_name = "capstone";
-
-        $conn = mysqli_connect($db_host,$db_user,$db_password,$db_name);
-
-        $userID =  $_POST["userID"];
-        $userPassword =  $_POST["userPassword"];
-        $userGender = $_POST["userGender"];
-        $userEmail = $_POST["userEmail"]; 
-        $userMajor =  $_POST["userMajor"];
-//        
-
-        $mysql_qry = "insert into USER(userID,userPassword,userGender,userEmail,userMajor) values('$userID','$userPassword','$userGender','$userEmail','$userMajor')";
-
-        mysqli_query($conn,$mysql_qry);
-
-	echo "success";
-//      if($conn->mysqli_query($mysql_qry) === TRUE)
-//      {
-//              echo "insert Successful";
-//      }
-//      else
-//              echo "Error: " . $mysql_qry . "<br>" . $conn->error;
-//      }
-
-//      $conn->close();
-
-        mysqli_close($conn);
+	$con = mysql_connect("localhost","db아이디","db비밀번호","db아이디");
+	
+	$userID = $_POST["userID"];
+	$userPassword = $_POST["userPassword"];
+	$userGender = $_POST["userGender"];
+	$userMajor = $_POST["userMajor"];
+	$userEmail = $_POST["userEmail"];
+	
+	$statement = mysqli_prepare($con,"INSERT INTO USER VALUES (?, ?, ?, ?, ?)");
+	mysqli_stmt_bind_param($statement, "sssss", $userID, $userPassword, $userGender, $userMajor, $userEmail);
+	mysqli_stmt_execute($statement);
+	
+	$response = array();
+	$response["success"] = true;
+	
+	echo json_encode($response);
 ?>
-
-
-
-
-
